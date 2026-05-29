9f8decb2caa8b347867f41c64e883022c1f588bc
J3y0r <savannapeterse880@gmail.com>
2026-05-29 21:01:57 +0800

    feat(velocity): add legit mode with hybrid jump-delay kb reduction
    - add LEGIT mode at index 5 combining jump and delay mechanics
    - set jumpFlag on packet queue to cancel initial knockback
    - set jumpFlag before delay release to cancel cached velocity packets
    - extend delayTicks and delayChance visibility to legit mode

--- src/main/java/myau/module/modules/Velocity.java
+++ src/main/java/myau/module/modules/Velocity.java
@@ -35,9 +35,9 @@ public class Velocity extends Module {
     private boolean shouldJump = false;
     private int jumpCooldown = 0;
 
-    public final ModeProperty mode = new ModeProperty("mode", 0, new String[]{"VANILLA", "JUMP", "DELAY", "REVERSE", "LEGIT_TEST"});
-    public final IntProperty delayTicks = new IntProperty("delay-ticks", 3, 1, 20, () -> this.mode.getValue() == 2);
-    public final PercentProperty delayChance = new PercentProperty("delay-chance", 100, () -> this.mode.getValue() == 2);
+    public final ModeProperty mode = new ModeProperty("mode", 0, new String[]{"VANILLA", "JUMP", "DELAY", "REVERSE", "LEGIT_TEST", "LEGIT"});
+    public final IntProperty delayTicks = new IntProperty("delay-ticks", 3, 1, 20, () -> this.mode.getValue() == 2 || this.mode.getValue() == 5);
+    public final PercentProperty delayChance = new PercentProperty("delay-chance", 100, () -> this.mode.getValue() == 2 || this.mode.getValue() == 5);
     public final PercentProperty chance = new PercentProperty("chance", 100);
     public final PercentProperty horizontal = new PercentProperty("horizontal", 0);
     public final PercentProperty vertical = new PercentProperty("vertical", 100);
@@ -83,7 +83,7 @@ public class Velocity extends Module {
             } else {
                 this.chanceCounter = this.chanceCounter % 100 + this.chance.getValue();
                 if (this.chanceCounter >= 100) {
-                    this.jumpFlag = (this.mode.getValue() == 1 || this.mode.getValue() == 2) && event.getY() > 0.0;
+                    this.jumpFlag = (this.mode.getValue() == 1 || this.mode.getValue() == 2 || this.mode.getValue() == 5) && event.getY() > 0.0;
                     this.delayActive = this.mode.getValue() == 3;
                     if (this.horizontal.getValue() > 0) {
                         event.setX(event.getX() * (double) this.horizontal.getValue() / 100.0);
@@ -111,6 +111,9 @@ public class Velocity extends Module {
                             || this.isInLiquidOrWeb()
                             || Myau.delayManager.getDelay() >= (long) this.delayTicks.getValue()
             )) {
+                if (this.mode.getValue() == 5) {
+                    this.jumpFlag = true;
+                }
                 Myau.delayManager.setDelayState(false, DelayModules.VELOCITY);
                 this.reverseFlag = false;
             }
@@ -161,7 +164,7 @@ public class Velocity extends Module {
                 S12PacketEntityVelocity packet = (S12PacketEntityVelocity) event.getPacket();
                 if (packet.getEntityID() == mc.thePlayer.getEntityId()) {
                     LongJump longJump = (LongJump) Myau.moduleManager.modules.get(LongJump.class);
-                    if (this.mode.getValue() == 2
+                    if ((this.mode.getValue() == 2 || this.mode.getValue() == 5)
                             && !this.reverseFlag
                             && !this.canDelay()
                             && !this.isInLiquidOrWeb()
@@ -174,6 +177,9 @@ public class Velocity extends Module {
                             Myau.delayManager.delayedPacket.offer(packet);
                             event.setCancelled(true);
                             this.reverseFlag = true;
+                            if (this.mode.getValue() == 5) {
+                                this.jumpFlag = true;
+                            }
                             return;
                         }
                     }
