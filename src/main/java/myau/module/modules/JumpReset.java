4d926d3cd8e4f0de5bddaead26495a199d5fc2ce
J3y0r <savannapeterse880@gmail.com>
2026-06-14 14:31:23 +0800

    feat(JumpReset): 添加 fake-check 属性，过滤非真实击退事件

--- src/main/java/myau/module/modules/JumpReset.java
+++ src/main/java/myau/module/modules/JumpReset.java
@@ -1,21 +1,27 @@
 package myau.module.modules;
 
 import myau.event.EventTarget;
+import myau.event.types.EventType;
 import myau.events.KnockbackEvent;
 import myau.events.LivingUpdateEvent;
+import myau.events.PacketEvent;
 import myau.mixin.IAccessorEntity;
 import myau.module.Module;
+import myau.property.properties.BooleanProperty;
 import myau.property.properties.PercentProperty;
 import myau.util.MoveUtil;
 import myau.util.RandomUtil;
 import net.minecraft.client.Minecraft;
+import net.minecraft.entity.Entity;
+import net.minecraft.network.play.server.S19PacketEntityStatus;
 import net.minecraft.potion.Potion;
 
 public class JumpReset extends Module {
     private static final Minecraft mc = Minecraft.getMinecraft();
-
+    public final PercentProperty chance = new PercentProperty("chance", 100, 20, 100, null);
+    public final BooleanProperty fakeCheck = new BooleanProperty("fake-check", true);
     private boolean jumpFlag = false;
-    public final PercentProperty chance = new PercentProperty("Chance", 100, 20, 100, null);
+    private boolean allowNext = true;
 
     public JumpReset() {
         super("JumpReset", false);
@@ -32,7 +38,13 @@ public class JumpReset extends Module {
 
     @EventTarget
     public void onKnockback(KnockbackEvent event) {
-        if (this.isEnabled()) {
+        if (!this.isEnabled() || event.isCancelled()) {
+            this.allowNext = true;
+            return;
+        }
+
+        if (!this.allowNext || !this.fakeCheck.getValue()) {
+            this.allowNext = true;
             if (mc.thePlayer.hurtTime >= 7) {
                 int random = RandomUtil.nextInt(1, 101);
                 int threshold = chance.getValue();
@@ -51,5 +63,23 @@ public class JumpReset extends Module {
                 mc.thePlayer.movementInput.jump = true;
             }
         }
+    }
+
+    @EventTarget
+    public void onPacket(PacketEvent event) {
+        if (isEnabled() && event.getType() == EventType.RECEIVE) {
+            if (event.getPacket() instanceof S19PacketEntityStatus) {
+                S19PacketEntityStatus packet = (S19PacketEntityStatus) event.getPacket();
+                Entity entity = packet.getEntity(mc.theWorld);
+                if (entity != null && entity.equals(mc.thePlayer) && packet.getOpCode() == 2) {
+                    this.allowNext = false;
+                }
+            }
+        }
+    }
+
+    @Override
+    public void onDisabled() {
+        this.allowNext = true;
     }
 }
